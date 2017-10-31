058f76ed0f2735c24a08d89b42beb3e5be2c9250
fio116 <31750361+fio116@users.noreply.github.com>
2017-10-31 16:07:40 -0500

    Add files via upload

---
+++ PmtInquiryRevACH Query.sql
@@ -0,0 +1,32 @@
+SELECT  BAT.BankTxnTID
+FROM	brMasBankAcctTxn BAT INNER JOIN brMasBankAcct BA  ON BAT.BankAcctTID = BA.BankAcctTID
+		LEFT JOIN brMasPmtMthd Pmt  ON BAT.BankTxnPmtMthdTID = Pmt.PmtMthdTID
+WHERE	(PMT.PmtProcessType = 'MmsPmtProcess'  
+		AND BAT.BankTxnType  IN ('ACHOUT', 'WIREOUT') 
+		--Corp 06
+		AND Pmt.PmtCorpHID IN (1066146)   
+		AND ISNULL(BAT.BankTxnComment, '') <> 'System Generated Void' 
+		AND BAT.BankTxnStatus = 'ISSUED')  
+		
+
+--Get Revenue Payments and put IDs in a table in the AppSupport database		
+SELECT	BAT.BankTxnTID
+INTO	AppSupport.dbo.RevenueACHPmts
+		--SUM(BankTxnAmount)
+FROM	brMasBankAcctTxn BAT INNER JOIN brMasBankAcct BA  ON BAT.BankAcctTID = BA.BankAcctTID
+		LEFT JOIN brMasPmtMthd Pmt  ON BAT.BankTxnPmtMthdTID = Pmt.PmtMthdTID
+WHERE	(PMT.PmtProcessType = 'RevPmtProcess'  
+		AND BAT.BankTxnType  IN ('ACHOUT') 
+		--Corp 06
+		AND Pmt.PmtCorpHID IN (1066146)   
+		AND ISNULL(BAT.BankTxnComment, '') <> 'System Generated Void' 
+		AND BAT.BankTxnStatus = 'ISSUED')  		
+		
+ 
+-- banktxntid 
+
+--Get currently used bank statuses
+--SELECT DISTINCT BANKTXNSTATUS FROM brMasBankAcctTxn
+
+
+
